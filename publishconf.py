b798332c3a45cd000db2832ee9c9cc84eb70bb97
koohyar <koohyar.hadjisalimi@gmail.com>
2022-11-06 10:29:00 +0000

    fixed call by reference in lexer input; typos; cleaned pages branch

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://koohyar.github.io/docs'
+SITEURL = ''
 
 # document-relative URLs when developing
 RELATIVE_URLS = True
